049cb8e1257daa4f22e825ac5a36c7f1140c8dec
chixiao <chixiao@qunhemail.com>
2019-08-29 23:15:13 +0800

    init project

---
+++ elasticsearcl-client/src/main/java/com/zx/learn/es/client/ElasticsearchClientApplication.java
@@ -0,0 +1,13 @@
+package com.zx.learn.es.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchClientApplication.class, args);
+    }
+
+}
---
+++ elasticsearcl-client/src/test/java/com/zx/learn/es/client/ElasticsearchClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zx.learn.es.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
